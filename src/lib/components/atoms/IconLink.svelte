57d3b1876844de0f5f13a3ee5336613ae0155e14
Jérémy Jousse <jeremy.jousse@decathlon.com>
2023-10-15 14:40:29 +0200

    feat: boostrap blog

---
+++ src/lib/components/atoms/IconLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let href: string;
+  export let target: "_self" | "_blank" = "_self";
+</script>
+
+<a
+  {href}
+  {target}
+  class=" rounded-lg text-gray-700 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-800"
+>
+  <slot />
+</a>
---
+++ src/lib/components/layout/NavItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let href: string;
+  export let target: "_self" | "_blank" = "_self";
+</script>
+
+<a
+  {href}
+  {target}
+  class="no-underline text-inherit p-2 rounded-lg text-white hover:bg-gray-600 h-8"
+  ><slot /></a
+>
