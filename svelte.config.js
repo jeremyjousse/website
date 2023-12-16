6d72b4a96eebcb7f5287c2228f15571dfc070e63
Jérémy Jousse <jeremy.jousse@decathlon.com>
2023-12-16 14:00:32 +0100

    chore: :package: update to sveltekit 2

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from "@sveltejs/adapter-static";
 import { mdsvex } from "mdsvex";
 import preprocess from "svelte-preprocess";
 import rehypeExternalLinks from "rehype-external-links";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
