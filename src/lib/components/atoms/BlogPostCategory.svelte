c96a98bfb45648703a3746b9c497adf9782814dc
Jérémy Jousse <jeremy.jousse@decathlon.com>
2025-06-15 08:52:01 +0200

    fix(blog): :ambulance: add base to category url

--- src/lib/components/atoms/BlogPostCategory.svelte
+++ src/lib/components/atoms/BlogPostCategory.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	type Props = {
 		slug: string;
 	};
 	const { slug }: Props = $props();
 </script>
 
-<a href={`/articles/${slug}`} class="">
+<a href={`${base}/articles/${slug}`} class="">
 	{slug}
 </a>
