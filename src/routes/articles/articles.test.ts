b4c7d161d6ebb640f3c3d05524bff5b53ea29e61
Jérémy Jousse <jeremy.jousse@decathlon.com>
2025-12-19 20:50:17 +0100

    test(blog): :bug: fix non passing test

--- src/routes/articles/articles.test.ts
+++ src/routes/articles/articles.test.ts
@@ -1,10 +1,23 @@
 import '@testing-library/jest-dom';
 
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 
 import Page from './+page.svelte';
 import { onePost } from 'test-setup/mocks/blogPostsMock';
+
+vi.mock('$app/stores', async () => {
+	const { readable } = await import('svelte/store');
+	return {
+		page: readable({
+			url: new URL('http://localhost')
+		})
+	};
+});
+
+vi.mock('$app/navigation', () => ({
+	goto: vi.fn()
+}));
 
 describe('articles', () => {
 	it('displays the right number of articles', () => {
